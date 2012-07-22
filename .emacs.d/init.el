40aaa626f4ceca0361128c9fe2fe661cb643b8f7
Samrat Man Singh <samratmansingh@gmail.com>
2012-07-22 13:09:14 +0545

    Remove emacs and use .emacs.d instead. Should be easier to use in the deploy.sh script

---
+++ .emacs.d/init.el
@@ -0,0 +1,46 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      color-theme
+                      clojure-mode
+                      evil
+                      nrepl)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;Use Molokai color-scheme
+(require 'color-theme)
+(color-theme-molokai)
+
+;Activate Evil mode at start up
+(require 'evil)
+(evil-mode 1)
+
+;4-clojure plugin
+(add-to-list 'load-path "~/.emacs.d/4clj-el/")
+(require 'four-clj)
+
+;switch between split-windows
+(define-key evil-normal-state-map (kbd "C-l") 'other-window)
+
+;;; esc quits
+
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
